4a62235b70ec56c3645f5d0eead27376adc18a24
TheScendant <hayden518@gmail.com>
2019-09-16 14:30:24 -0600

    getUserData

---
+++ server/getUserData.js
@@ -0,0 +1,26 @@
+import {fetchAndJsonify} from 'utilities';
+import { getUser } from './urls.js';
+
+async function getAllUserData(url) {
+  const jsonBlob = await fetchAndJsonify(url);
+  // dosomething? try catch here?
+  return jsonBlob;
+}
+
+async function networkInit(data, DATA_TYPE) {
+  try {
+    if (DATA_TYPE === DATA_TYPE_ENUM.EMAIL) {
+      tempURL = `${getUser}&email=${data}`;
+    } else if (DATA_TYPE === DATA_TYPE_ENUM.USER_ID) {
+      tempURL = `${getUser}&userId=${data}`;
+    }
+    const userData = await getAllUserData(tempURL);
+    return userData;
+  } catch(e) {
+    console.error(e);
+  }
+}
+
+export {
+    networkInit,
+};
--- server/urls.js
+++ server/urls.js
@@ -2,10 +2,11 @@ const key = "111744118-7abbfddc21426215a14c10862548b379";
 const mpURL = `https://www.mountainproject.com/data/`;
 const getTicks = `${mpURL}get-ticks?key=${key}`;
 const getRoutes = `${mpURL}get-routes?key=${key}`;
-
+const getUser = `${mpURL}get-user?key=${key}`;
 //const TheScendantURL = `https://www.mountainproject.com/data/get-ticks?userId=112023884&key=${key}`;
 
 export {
-  getTicks,
   getRoutes,
+  getTicks,
+  getUser,
 }
