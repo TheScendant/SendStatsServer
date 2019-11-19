8649a955dfc169b344b80b30de0568a28f6d6d01
Hayden M Nier <hayden518@gmail.com>
2019-11-18 17:45:23 -0700

    Changes dates

--- server/cachedJSONs/allGrades.js
+++ server/cachedJSONs/allGrades.js
@@ -1,138 +1,310 @@
 export default [{
-  date: '2019-08-25',
+  date: '2011-01-25',
   rating: '5.10a',
   routeId: 1
 },{
-  date: '2019-08-25',
+  date: '2011-02-25',
   rating: '5.10b',
   routeId: 2
 },{
-  date: '2019-08-25',
+  date: '2009-02-25',
+  rating: '5.10b',
+  routeId: 55
+},{
+  date: '2009-02-25',
+  rating: '5.10b',
+  routeId: 56
+},{
+  date: '2009-02-25',
+  rating: '5.10b',
+  routeId: 57
+},{
+  date: '2009-02-25',
+  rating: '5.10b',
+  routeId: 58
+},{
+  date: '2009-02-25',
+  rating: '5.10b',
+  routeId: 59
+},{
+  date: '2009-02-25',
+  rating: '5.10b',
+  routeId: 60
+},{
+  date: '2009-02-25',
+  rating: '5.10b',
+  routeId: 61
+},{
+  date: '2009-02-25',
+  rating: '5.10b',
+  routeId: 62
+},{
+  date: '2009-02-25',
+  rating: '5.10b',
+  routeId: 63
+},{
+  date: '2009-02-25',
+  rating: '5.10b',
+  routeId: 64
+},{
+  date: '2009-02-25',
   rating: '5.10c',
-  routeId: 3
+  routeId: 65
 },{
-  date: '2019-08-25',
+  date: '2009-02-25',
   rating: '5.10d',
-  routeId: 4
+  routeId: 66
 },{
-  date: '2019-08-25',
+  date: '2009-02-25',
+  rating: '5.9',
+  routeId: 67
+},{
+  date: '2009-02-25',
+  rating: '5.14c',
+  routeId: 68
+},{
+  date: '2009-02-25',
+  rating: '5.10b',
+  routeId: 69
+},{
+  date: '2009-02-25',
+  rating: '5.10b',
+  routeId: 70
+},{
+  date: '2009-02-25',
+  rating: '5.10b',
+  routeId: 71
+},{
+  date: '2009-02-25',
+  rating: '5.10b',
+  routeId: 72
+},{
+  date: '2010-02-25',
+  rating: '5.10b',
+  routeId: 73
+},{
+  date: '2007-02-25',
+  rating: '5.10b',
+  routeId: 74
+},{
+  date: '2011-02-25',
+  rating: '5.10b',
+  routeId: 75
+},{
+  date: '2011-03-25',
+  rating: '5.10c',
+  routeId: 76
+},{
+  date: '2011-04-25',
+  rating: '5.10d',
+  routeId: 77
+},{
+  date: '2011-05-25',
   rating: '5.11a',
   routeId: 5
 },{
-  date: '2019-08-25',
+  date: '2011-06-25',
   rating: '5.11b',
   routeId: 6
 },{
-  date: '2019-08-25',
+  date: '2011-07-25',
   rating: '5.11c',
   routeId: 7
 },{
-  date: '2019-08-25',
+  date: '2011-08-25',
   rating: '5.11d',
   routeId: 8
 },{
-  date: '2019-08-25',
+  date: '2011-09-25',
   rating: '5.12a',
   routeId: 9
 },{
-  date: '2019-08-25',
+  date: '2011-10-25',
   rating: '5.12b',
   routeId: 10
 },{
-  date: '2019-08-25',
+  date: '2011-11-25',
   rating: '5.12c',
   routeId: 11
 },{
-  date: '2019-08-25',
+  date: '2011-12-25',
   rating: '5.12d',
   routeId: 12
 },{
-  date: '2019-08-25',
+  date: '2012-01-25',
   rating: '5.13a',
   routeId: 13
 },{
-  date: '2019-08-25',
+  date: '2012-02-25',
   rating: '5.13b',
   routeId: 14
 },{
-  date: '2019-08-25',
+  date: '2012-03-25',
   rating: '5.13c',
   routeId: 15
 },{
-  date: '2019-08-25',
+  date: '2012-04-25',
   rating: '5.13d',
   routeId: 16
 },{
-  date: '2019-08-25',
+  date: '2012-05-25',
   rating: '5.14a',
   routeId: 17
 },{
-  date: '2019-08-25',
+  date: '2012-06-25',
   rating: '5.14b',
   routeId: 18
 },{
-  date: '2019-08-25',
+  date: '2012-07-25',
   rating: '5.14c',
   routeId: 19
 },{
-  date: '2019-08-25',
+  date: '2012-08-25',
   rating: '5.14d',
   routeId: 20
 },{
-  date: '2019-08-25',
+  date: '2012-09-25',
   rating: '5.1',
   routeId: 21
 },{
-  date: '2019-08-25',
+  date: '2012-10-25',
   rating: '5.2',
   routeId: 22
 },{
-  date: '2019-08-25',
+  date: '2012-11-25',
   rating: '5.3',
   routeId: 23
 },{
-  date: '2019-08-25',
+  date: '2012-12-25',
   rating: '5.4',
   routeId: 24
 },{
-  date: '2019-08-25',
+  date: '2013-01-25',
   rating: '5.5',
   routeId: 25
 },{
-  date: '2019-08-25',
+  date: '2013-02-25',
   rating: '5.6',
   routeId: 26
 },{
-  date: '2019-08-25',
+  date: '2013-04-25',
   rating: '5.7',
   routeId: 27
 },{
-  date: '2019-08-25',
+  date: '2013-05-25',
   rating: '5.8',
   routeId: 28
 },{
-  date: '2019-08-25',
+  date: '2013-06-25',
   rating: '5.9',
   routeId: 29
 },{
-  date: '2019-08-25',
+  date: '2013-07-25',
   rating: '5.10-',
   routeId: 30
 },{
-  date: '2019-08-25',
+  date: '2013-08-25',
   rating: '5.10a/b',
   routeId: 31
 },{
-  date: '2019-08-25',
+  date: '2013-09-25',
   rating: '5.10b/c',
   routeId: 32
 },{
-  date: '2019-08-25',
+  date: '2013-10-25',
   rating: '5.10c/d',
   routeId: 33
 },{
-  date: '2019-08-25',
+  date: '2013-11-25',
   rating: '5.10+',
   routeId: 34
+},{
+  date: '2013-12-25',
+  rating: '5.11-',
+  routeId: 35
+},{
+  date: '2014-01-25',
+  rating: '5.11a/b',
+  routeId: 36
+},{
+  date: '2014-02-25',
+  rating: '5.11b/c',
+  routeId: 37
+},{
+  date: '2014-03-25',
+  rating: '5.11c/d',
+  routeId: 38
+},{
+  date: '2014-08-25',
+  rating: '5.11+',
+  routeId: 39
+},{
+  date: '2014-04-25',
+  rating: '5.12-',
+  routeId: 40
+},{
+  date: '2014-05-25',
+  rating: '5.12a/b',
+  routeId: 41
+},{
+  date: '2014-06-25',
+  rating: '5.12b/c',
+  routeId: 42
+},{
+  date: '2014-07-25',
+  rating: '5.12c/d',
+  routeId: 43
+},{
+  date: '2014-08-25',
+  rating: '5.12+',
+  routeId: 44
+},{
+  date: '2014-09-25',
+  rating: '5.13-',
+  routeId: 45
+},{
+  date: '2014-10-25',
+  rating: '5.13a/b',
+  routeId: 46
+},{
+  date: '2014-11-25',
+  rating: '5.13b/c',
+  routeId: 47
+},{
+  date: '2014-12-25',
+  rating: '5.13c/d',
+  routeId: 48
+},{
+  date: '2015-01-25',
+  rating: '5.13+',
+  routeId: 49
+},{
+  date: '2015-02-25',
+  rating: '5.14-',
+  routeId: 50
+},{
+  date: '2015-03-25',
+  rating: '5.14a/b',
+  routeId: 51
+},{
+  date: '2015-01-25',
+  rating: '5.14b/c',
+  routeId: 52
+},{
+  date: '2015-01-25',
+  rating: '5.14c/d',
+  routeId: 53
+},{
+  date: '2015-01-25',
+  rating: '5.14+',
+  routeId: 54
+},{
+  date: '2015-01-25',
+  rating: '5.14+',
+  routeId: 3
+},{
+  date: '2015-01-25',
+  rating: '5.14+',
+  routeId: 4
 }
 ];
